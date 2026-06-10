e7894da511c9048dd31c0f72119fc0f4245ea7f6
Hà Bích Ngọc <ngoc01072006@gmail.com>
2026-06-10 08:23:45 +0700

    Update BTTH.py

--- session13/BTTH.py
+++ session13/BTTH.py
@@ -1,8 +1,9 @@
 danh_sach_nhan_vien = []
+id_tu_dong = 101
 
 while True:
     print("\n" + "="*35)
-    print("QUẢN LÝ NHÂN SỰ - STAFF MANAGER")
+    print("   QUẢN LÝ NHÂN SỰ - STAFF MANAGER    ")
     print("="*35)
     print("1. Thêm nhân viên mới")
     print("2. Danh sách nhân viên")
@@ -15,36 +16,27 @@ while True:
     if choice == "1":
         print("\n--- THÊM NHÂN VIÊN MỚI ---")
 
-        while True:
-            ma_nv = input("Nhập mã nhân viên: ").strip()
-            if not ma_nv:
-                print("Mã nhân viên không được để trống!")
-                continue
-            trung = False
-            for i in danh_sach_nhan_vien:
-                if i['id'] == ma_nv:
-                    trung = True
-                    break
-            if trung:
-                print(f"Mã ID '{ma_nv}' đã tồn tại! Vui lòng nhập mã khác.")
-            else:
-                break
+        ma_nv = id_tu_dong
         while True:
             ten_nv = input("Nhập họ và tên nhân viên: ").strip()
             if ten_nv:
                 break
             print("Họ tên không được để trống!")
         while True:
-            luong_input = input("Nhập mức lương của nhân viên: ").strip()
-            luong = int(luong_input)
-            if luong > 0:
+            luong_input = input("Nhập mức lương: ").strip()
+            if not luong_input:
+                print("Mức lương không được để trống. Vui lòng nhập lại.")
+                continue
+            luong_nv = float(luong_input)
+            if luong_nv > 0:
                 break
             else:
-                print("Lương phải là số lớn hơn 0!")
+                print("Mức lương phải là số dương (> 0). Vui lòng nhập lại.")
         nhan_vien_moi = {
             "id": ma_nv,
             "name": ten_nv,
-            "luong": luong
+            "luong": luong_nv
         }
         danh_sach_nhan_vien.append(nhan_vien_moi)
-        print(f"Thêm thành công nhân viên: {ten_nv} (ID: {ma_nv})")
+        print(f"Thêm nhân viên thành công! ID: {ma_nv}")
+        id_tu_dong += 1
