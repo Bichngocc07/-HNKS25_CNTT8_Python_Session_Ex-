2406874be49e0ae037e01461ca60d58cd17c2871
Hà Bích Ngọc <ngoc01072006@gmail.com>
2026-06-16 10:02:38 +0700

    Update bktr1.py

--- session18/bktr1.py
+++ session18/bktr1.py
@@ -10,7 +10,7 @@ def xem_sanpham(products_list):
         print("KHÔNG CÓ SẢN PHẨM NÀO")
     else:
         print(f"{'ID':<6} | {'Tên sản phẩm':<15} | {'Giá bán':<10}")
-        print("-" * 37) # In đường kẻ phân cách
+        print("-" * 37) 
         
         for sp in products_list:
             print(f"{sp['id']:<6} | {sp['name']:<15} | {sp['price']:<10}")
@@ -26,20 +26,19 @@ while True:
     print("1. Xem danh sách sản phẩm hiện có    ")
     print("2. Thêm mới một sản phẩm             ")
     print("3. Cập nhật giá sản phẩm theo ID     ")
-    print("4. Thoát chương trình                ") # Sửa đổi chữ từ text của bạn cho rõ ràng
+    print("4. Thoát chương trình                ") 
     print("=====================================")
 
-    # Nhận lựa chọn và dùng .strip() để xóa khoảng trắng thừa
     choice = input("Mời bạn chọn chức năng (1-4) : ").strip()
     
     if choice == "1":
-        xem_sanpham(products_list) # Truyền biến toàn cục 'products' vào hàm
+        xem_sanpham(products_list) 
     elif choice == "2":
-        them_sanpham(products_list) # Truyền biến toàn cục 'products' vào hàm
+        them_sanpham(products_list) 
     elif choice == "3":
-        capnhat_gia(products_list) # Gọi hàm cập nhật giá vừa viết thêm
-    elif choice == "4":       # SỬA LỖI: Đề bài yêu cầu số 4 là thoát chương trình thay vì số 8
+        capnhat_gia(products_list) 
+    elif choice == "4":      
         print("ĐÃ THOÁT CHƯƠNG TRÌNH !")
-        break # Bẻ gãy vòng lặp while True để dừng chương trình
+        break 
     else:
         print("Lựa chọn không hợp lệ! Vui lòng chọn từ 1 đến 4.")
